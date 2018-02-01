e1c0bf9f1d68a84a881835f1dcf74b8963268fec
Youssef <agy292@email.vccs.edu>
2018-02-01 14:19:32 -0500

    Class Excercise

---
+++ Exceptions.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.util.InputMismatchException;
+public class Exceptions
+{
+	public static void main(String[] args)
+	{
+		System.out.println("Enter any amount of integers. Enter 0 to obtain sum");
+		int total = sum();
+		System.out.println(total);
+	}
+	public static int sum()
+	{
+		try{
+		int s = 0;
+		Scanner input = new Scanner(System.in);
+		int current = input.nextInt();
+		while(current != 0)
+		{
+			s = s + current;
+			current = input.nextInt();
+		}
+			return s;
+		}	
+		catch(InputMismatchException e)
+		{
+			System.out.println("Not an integer, please try again.");
+			
+		}
+
+return 0;
+
+		
+	}
+}
